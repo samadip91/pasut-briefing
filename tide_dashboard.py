917741c864689a742415ab4bd561b741f44424a2
samadip91 <45196107+samadip91@users.noreply.github.com>
2024-09-02 14:57:42 +0700

    Update tide_dashboard.py

--- tide_dashboard.py
+++ tide_dashboard.py
@@ -27,15 +27,16 @@ st.markdown(margins_css, unsafe_allow_html=True)
 # Set the image as the header with a caption and custom width
 #st.image(icon_url, width=150)
 # Streamlit title and subtitle
-st.title('Tabel dan Grafik Pasang Surut')
-st.write("Briefing Pusat Meteorologi Maritim")
+#st.title('Tabel dan Grafik Pasang Surut')
+#st.write("Briefing Pusat Meteorologi Maritim")
 
 # Display the image and title
-#col1, col2 = st.columns([1, 4])
-#with col1:
-#    st.image(icon_url , width=150)
-#with col2:
-#    st.title("Tabel dan Grafik Pasang Surut")
+col1, col2 = st.columns([1, 4])
+with col1:
+    st.image(icon_url , width=150)
+with col2:
+    st.title("Tabel dan Grafik Pasang Surut")
+    st.write("Briefing Pusat Meteorologi Maritim")
 
 col1, col2 = st.columns([1, 5])
 
