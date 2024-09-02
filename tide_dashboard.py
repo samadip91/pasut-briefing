953cb866e26b23fbbd388547d1b69e7b5396b403
samadip91 <45196107+samadip91@users.noreply.github.com>
2024-09-02 15:12:00 +0700

    Update tide_dashboard.py

--- tide_dashboard.py
+++ tide_dashboard.py
@@ -31,9 +31,9 @@ st.markdown(margins_css, unsafe_allow_html=True)
 #st.write("Briefing Pusat Meteorologi Maritim")
 
 # Display the image and title
-col1, col2 = st.columns([1, 7])
+col1, col2 = st.columns([1, 9])
 with col1:
-    st.image(icon_url , width=100)
+    st.image(icon_url , width=70)
 with col2:
     st.title("Tabel dan Grafik Pasang Surut")
     st.write("Briefing Pusat Meteorologi Maritim")
